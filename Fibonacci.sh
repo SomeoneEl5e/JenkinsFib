427d4a885abf46da6e9ae2af5d2ec850cc544a70
SomeoneEl5e <93217023+SomeoneEl5e@users.noreply.github.com>
2024-06-02 20:28:20 +0300

    Update Fibonacci.sh

--- Fibonacci.sh
+++ Fibonacci.sh
@@ -5,19 +5,6 @@ a=1
 b=1
 max_limit=9223300000000000000
 
-# Prompt the user for input and validate it
-
-echo "Please enter a positive integer up to $max_limit:" 
-read -p "> " userInput
-while ! [[ $userInput =~ ^[0-9]+$ ]] || [ $userInput -gt $max_limit ];
-do
-    echo "Invalid input. Please enter a positive integer up to $max_limit."
-    read -p "> " userInput
-done
-
-# Convert userInput to an integer
-userInput=$((userInput))
-
 if [ $userInput -eq 1 ]; then
     echo "The number is in the 1 and 2 position in the Fibonacci series"
 else
